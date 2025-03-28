54aef22184577e98a0f361a61568128b114d72a7
ashfaque <rahamankmashfaque@gmail.com>
2025-03-29 03:02:15 +0600

    reorderList

--- Reorderlist/reorderList.py
+++ Reorderlist/reorderList.py
@@ -4,15 +4,13 @@ arr=[1,2,3,4,5,6,7]
 res_arr=[]
 
 def reorderList(arr):
-    beginning=0
-    ending=len(arr)-1
+    beginning, ending = 0, len(arr)-1
+    
     
     while beginning <= ending and len(res_arr)!=len(arr):
 
         if beginning == ending:
             res_arr.append(arr[beginning])
-
-    
         else:
             res_arr.append(arr[beginning])
             res_arr.append(arr[ending])
